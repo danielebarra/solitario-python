09b534d5f1e2957266ca42247838d1325de15e5c
Daniele Barra <barrdaniele51@gmail.com>
2025-06-24 22:20:25 +0200

    Add main game logic and card management for solitaire

---
+++ danny_solitario.py
@@ -0,0 +1,368 @@
+from random import shuffle
+import msvcrt
+import os
+from termcolor import colored
+
+red = 'red'
+black = (65, 105, 225)
+
+selezionabili = {
+        0: "C1",
+        1: "C2",
+        2: "C3",
+        3: "C4",
+        4: "C5",
+        5: "C6",
+        6: "C7",
+        8: "CR",
+        9: "CF1",
+        10: "CF2",
+        11: "CF3",
+        12: "CF4",
+        13: "ANNULLA"
+    }
+
+# Classe di Carta definisce come è composta ogni carta
+class Carta:
+    def __init__(self, valore, seme):
+        # Valore della carta
+        self.valore = valore
+        
+        # AltValore è un valore alternativo con uno scopo visivo, per rendere all'utente la visualizzazione della carta secondo le figure
+        valori_speciali = {1: "A", 11: "J", 12: "Q", 13: "K"}
+        self.altvalore = valori_speciali.get(self.valore, str(self.valore))
+        
+        # Seme della carta
+        self.seme = seme
+        
+        # Colore della carta
+        if seme in ["Cuore", "Diamante"]:
+            self.colore = "rosso"
+        else:
+            self.colore = "nero"
+        
+        # Definisce se la carta è visibile o no
+        self.scoperta = False
+        
+        
+    def __repr__(self):
+        return self.altvalore + " di " + self.seme
+
+
+# Genera il mazzo di carte
+def Generate_Deck():
+
+    # Definisce i colori del mazzo di carte
+    semi = ['Cuore', 'Diamante', 'Picche', 'Fiore']
+
+    # Crea il mazzo di carte
+    deck = [Carta(valore, seme) for valore in range (1, 14) for seme in semi]
+    
+    Shuffle_Deck(deck)
+    
+    return deck
+
+# Mescola il mazzo di carte
+def Shuffle_Deck(deck):
+    
+    shuffle(deck)
+    
+    return deck
+
+# Genera le colonne di gioco
+def Crea_Colonne(deck):
+    colonne = [[] for _ in range(13)] 
+
+    # 0, 1, 2, 3, 4, 5, 6 COLONNE DI GIOCO
+    # 7 MAZZO RISERVA PIENO
+    # 8 MAZZO RISERVA VUOTO
+    # 9, 10, 11, 12 MAZZI FINALI 
+
+    n = 0
+
+    for i in range(7):
+        for j in range(i + 1):
+            deck[n].scoperta = (j == i)
+            colonne[i].append(deck[n])
+            n += 1
+        print(f"Colonna {i+1}: {colonne[i]}")
+    
+    for i in range (28, 52):
+        colonne[7].append(deck[i])
+        deck[i].scoperta = True
+    print(f"Colonna {8}: {colonne[7]}")
+        
+    
+    return colonne
+        
+    
+def Stampa_Colonne(colonne, isPlayable):
+    os.system('cls||clear')
+
+    print()
+    print()
+
+    print("Mazzo di Riserva: ")
+
+    print("CR: ", end="")
+    for i in range(len(colonne[8])):
+        carta = colonne[8][i]
+        if carta.colore == "rosso":
+            print(colored(carta, red), end="")
+        else:
+            print(colored(carta, black), end="")
+        if i < len(colonne[8]) - 1:
+            print(" | ", end="")
+    
+    print()
+    print()
+    print("Mazzi Finali:")
+    
+    for i in range(9, 13):
+        print(f"CF{i - 8}: ", end="")
+        if len(colonne[i]) == 0:
+            print("VUOTA", end="")
+        else:
+            for j in range(len(colonne[i])):
+                carta = colonne[i][j]
+
+                if carta.colore == "rosso":
+                    print(colored(carta, red), end="")
+                else:
+                    print(colored(carta, black), end="")
+
+                if j < len(colonne[i]) - 1:
+                    print(" | ", end="")
+        print()
+    
+    print()
+    print("Colonne di gioco:")
+    
+    for i in range(len(colonne) - 6):
+        print("C", i + 1, ": ", sep="", end="")
+        
+        for j in range(len(colonne[i])):
+            carta = colonne[i][j]
+            if carta.scoperta:
+                if carta.colore == "rosso":
+                    print(colored(carta, red), end="")
+                else:
+                    print(colored(carta, black), end="")
+            else:
+                print("NASCOSTA", end="")
+
+            if j < len(colonne[i]) - 1:
+                print(" | ", end="")
+                
+        print("")
+    
+    if isPlayable:
+        print("\nComandi: 1: SPOSTA, 2: PESCA, 3: ESCI")
+        
+        while(True):
+            #try:
+            x = msvcrt.getch().decode("utf-8").lower()
+            if x == '1':
+                Sposta(colonne)
+                break
+            elif x == '2':
+                Pesca(colonne)
+                break
+            #except UnicodeDecodeError:
+            #    pass
+
+
+def Pesca(colonne):
+
+    if len(colonne[8]) != 0:
+        colonne[7].append(colonne[8][0])
+        colonne[8].pop()
+    
+    colonne[8].append(colonne[7][0])
+    del colonne[7][0]
+    
+    Stampa_Colonne(colonne, True)
+
+        
+def Check_Sposta(x, y, index, colonne):
+    if x == y:
+        print("Non puoi muovere nella STESSA colonna")
+        return False
+    
+    if y == 8:
+        print("Non puoi muovere all'interno del mazzo di RISERVA")
+        return False
+
+    carta_x = colonne[x][index]
+    
+    if len(colonne[y]) != 0: 
+        carta_y = colonne[y][-1]
+    else:
+        if carta_x.valore == 13:
+            return True
+        else:
+            print("Puoi spostare solo i RE nelle caselle vuote")
+            return False
+
+    if carta_x.valore == carta_y.valore - 1:
+        if carta_x.colore != carta_y.colore:
+            return True
+        else:
+            print("Devi spostarla ad una col colore opposto")
+    else:
+        print("Devi spostarla ad una col valore immediatamente inferiore")
+        
+    return False
+            
+
+def Sposta_Colonna(colonna):
+    spostabili = []
+
+    for i in colonna:
+        if i.scoperta:
+            spostabili.append(i)
+    
+    print("Da quale carta vuoi iniziare a spostare? (FRECCE e INVIO per selezionare)")
+
+
+    LINE_CLEAR = '\x1b[2K' # <-- ANSI sequence
+    index = 0
+
+    print(spostabili[index], end='\r')
+
+    while True:
+        x = msvcrt.getch()
+
+        if x == b'\xe0':
+                    x = msvcrt.getch()
+                    if x == b'K':
+                        if index == 0:
+                            index = len(spostabili) - 1
+                        else:
+                            index -= 1
+                        print(end=LINE_CLEAR)
+                        print(spostabili[index], end='\r')
+
+                    elif x == b'M':
+                        if index == len(spostabili) - 1:
+                            index = 0
+                        else:
+                            index += 1
+                        print(end=LINE_CLEAR)
+                        print(spostabili[index], end='\r')
+
+        elif x == b'\r':
+            print(end=LINE_CLEAR)
+            break
+
+    print(spostabili[index])
+    
+    removeIndex = index - len(spostabili)
+
+    return removeIndex
+
+
+def GetSpostaInput():
+    
+
+    LINE_CLEAR = '\x1b[2K' # <-- ANSI sequence
+    index = 0
+
+    print(selezionabili[index], end='\r')
+
+    while True:
+        x = msvcrt.getch()
+
+        if x == b'\xe0':
+            x = msvcrt.getch()
+
+            if x == b'H':   # Freccette in alto
+                if index == 0:
+                    index = 13
+                elif index == 8:
+                    index = 6
+                else:
+                    index -= 1
+                
+
+            elif x == b'P': # Freccette in basso
+                if index == 13:
+                    index = 0
+                elif index == 6:
+                    index = 8
+                else:
+                    index += 1
+        elif x == b'\r':
+            return index
+
+        print(end=LINE_CLEAR)
+        print(selezionabili[index], end='\r')
+         
+
+def Sposta(colonne):
+    Stampa_Colonne(colonne, False)
+    
+    
+    while True:
+        print("\nChe colonna vuoi spostare?")
+
+        x = GetSpostaInput()
+
+        if x == 13:
+            Stampa_Colonne(colonne, True)
+
+        print("Hai scelto la colonna", selezionabili[x])
+
+        j = 0
+        for i in colonne[x]:
+            if i.scoperta:
+                j += 1
+            if j > 1:
+                removeIndex = Sposta_Colonna(colonne[x])
+                break
+            else:
+                removeIndex = -1
+            
+
+        print("\nIn che colonna la vuoi spostare?")
+
+        y = GetSpostaInput()
+
+        if y == 13:
+            Stampa_Colonne(colonne, True)
+            
+        print("Hai scelto la colonna", selezionabili[y], end="\n\n")
+
+
+        if Check_Sposta(x, y, removeIndex, colonne):
+            for i in range(removeIndex, 0):
+                colonne[y].append(colonne[x][i])
+                colonne[x].pop(i)
+
+                if len(colonne[x]) != 0:
+                    colonne[x][i].scoperta = True
+
+            break
+            
+    Stampa_Colonne(colonne, True)
+    
+                # FATTO: (FARE IL SISTEMA DI SPOSTAMENTO NELLE CASELLE VUOTE (RE))
+                # FARE IL SISTEMA DI PESCA
+                # RIPENSARE AL SISTEMA DI SELEZIONE COLONNA DA SPOSTARE
+                # POSSIBILMENTE CON LE FRECCETTE
+
+
+    
+    
+
+def MainGame():
+    deck = Generate_Deck()
+    colonne = Crea_Colonne(deck)
+    Stampa_Colonne(colonne, True)
+    
+        
+ 
+MainGame()
+    
+
+
